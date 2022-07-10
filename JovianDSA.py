90cda7372049b6cf93cc5f2e902fdaffe14104b4
kashishkumar <kashish31894@gmail.com>
2022-07-10 14:40:05 +0530

    Added linear complexity solution to rotate array problem

--- JovianDSA.py
+++ JovianDSA.py
@@ -61,32 +61,67 @@ def test_cases_b():
         {"input": {"cards": [0, 1, 3, 4, 7, 11, 12, 13], "query": 19}, "output": -1}
     )
     tests.append({"input": {"cards": [], "query": 7}, "output": -1})
+    tests.append(
+        {"input": {"cards": [0, 1, 4, 7, 7, 7, 9, 10], "query": 7}, "output": 3}
+    )
     return tests
 
+
 tests_b = test_cases_b()
+
+
+def verify_middle(cards, query, middle):
+    if cards[middle] == query:
+        if cards[middle - 1] == query and middle - 1 >= 0:
+            return "left"
+        else:
+            return "found"
+    elif cards[middle] < query:
+        return "left"
+    elif cards[middle] > query:
+        return "right"
+
+
 def locate_cards_b(cards, query):
     if len(cards) == 0:
         return -1
     else:
         left, right = 0, len(cards) - 1
-        #print("left: {}".format(left), "right: {}".format(right))
         while left <= right:
             middle = (left + right) // 2
-            #print(
-            #   "left: {}".format(left),
-            #   "right: {}".format(right),
-            #    "middle {}".format(middle),
-            #)
-            if cards[middle] == query:
-                #print("found")
+            ismiddle = verify_middle(cards, query, middle)
+            if ismiddle == "found":
                 return middle
                 break
-            elif cards[middle] < query:
+            elif ismiddle == "left":
                 left = middle + 1
             else:
                 right = middle - 1
         return -1
 
+
 evaluate_test_cases(locate_cards_b, tests_b)
 
+# Rotate arrays
 
+
+def count_rotations(nums):
+    if len(nums) == 0:
+        return -1
+    else:
+        return locate_cards_b(nums, min(nums))
+
+
+def test_cases_rotate_array():
+    tests = []
+    tests.append({"input": {"nums": [5, 6, 9, 0, 2, 3, 4]}, "output": 3})
+    tests.append({"input": {"nums": [0, 2, 3, 4]}, "output": 0})
+    tests.append({"input": {"nums": []}, "output": -1})
+    return tests
+
+
+tests_rotate = test_cases_rotate_array()
+
+evaluate_test_cases(count_rotations, tests_rotate)
+
+# complexity is linear and so need to find a different solution
